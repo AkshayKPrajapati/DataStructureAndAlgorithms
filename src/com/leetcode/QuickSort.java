e3c556ed595efa238ffc1a41b080603a293fe460
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2025-09-15 23:27:08 +0530

    QuickSort

---
+++ src/com/leetcode/QuickSort.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                " "
+        );
+    }
+}
---
+++ src/com/leetcode/SortArray.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+    public static void main(String[] args) {
+
+        int [] array = {23,45,45,34,566,7564,67453,1323,345};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
